91a51566756cc4a35c077ece123c65adf0346de5
Dana <doneil1580@gmail.com>
2018-11-03 15:31:19 -0400

    changed devise routes for signup and login and provided CSS to the forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: "rooms#index"
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
